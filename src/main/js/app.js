aa7fc362e37b8f00b6f89c032df6c1d4bd04902f
Lewis Sneddon <lewis@lewiscodes.io>
2021-09-21 10:25:15 +0100

    db and react

---
+++ src/main/js/app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// tag::vars[]
+const React = require('react'); // <1>
+const ReactDOM = require('react-dom'); // <2>
+const client = require('./client'); // <3>
+// end::vars[]
+
+// tag::app[]
+class App extends React.Component { // <1>
+
+    constructor(props) {
+        super(props);
+        // this.state = {employees: []};
+    }
+
+    componentDidMount() { // <2>
+        // client({method: 'GET', path: '/api/employees'}).done(response => {
+        //     this.setState({employees: response.entity._embedded.employeeList});
+        // });
+    }
+
+    render() { // <3>
+        return (
+            // <EmployeeList employees={this.state.employees}/>
+            <h2>Hey React</h2>
+        )
+    }
+}
+// end::app[]
+
+// tag::employee-list[]
+// class EmployeeList extends React.Component{
+//     render() {
+//         const employees = this.props.employees.map(employee =>
+//             <Employee key={employee._links.self.href} employee={employee}/>
+//         );
+//         return (
+//             <table>
+//                 <tbody>
+//                 <tr>
+//                     <th>Name</th>
+//                     <th>Role</th>
+//                 </tr>
+//                 {employees}
+//                 </tbody>
+//             </table>
+//         )
+//     }
+// }
+// end::employee-list[]
+
+// tag::employee[]
+// class Employee extends React.Component{
+//     render() {
+//         return (
+//             <tr>
+//                 <td>{this.props.employee.name}</td>
+//                 <td>{this.props.employee.role}</td>
+//             </tr>
+//         )
+//     }
+// }
+// end::employee[]
+
+// tag::render[]
+ReactDOM.render(
+    <App />,
+    document.getElementById('react')
+)
+// end::render[]

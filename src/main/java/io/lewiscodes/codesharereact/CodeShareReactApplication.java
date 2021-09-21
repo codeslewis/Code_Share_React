01f04a548195c808b17133b1e6502ced902728e1
Lewis Sneddon <lewis@lewiscodes.io>
2021-09-21 10:14:27 +0100

    Initial commit

---
+++ src/main/java/io/lewiscodes/codesharereact/CodeShareReactApplication.java
@@ -0,0 +1,13 @@
+package io.lewiscodes.codesharereact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeShareReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeShareReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lewiscodes/codesharereact/CodeShareReactApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lewiscodes.codesharereact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeShareReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

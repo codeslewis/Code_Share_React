9a7018cf2ef4761f44de69db9771c1279eeb425a
Lewis Sneddon <lewis@lewiscodes.io>
2021-09-23 09:19:55 +0000

    testing controller

--- src/test/java/io/lewiscodes/codesharereact/CodeShareReactApplicationTests.java
+++ src/test/java/io/lewiscodes/codesharereact/CodeShareReactApplicationTests.java
@@ -1,13 +1,24 @@
 package io.lewiscodes.codesharereact;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class CodeShareReactApplicationTests {
+    
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    void getLatestStatus() throws Exception {
+        this.mockMvc.perform(get("/api/code/latest")).andExpect(status().isOk());
     }
 
 }
